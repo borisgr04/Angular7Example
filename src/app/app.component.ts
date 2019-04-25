250fb7c90eacc471abda9b703c172cadde3d788f
Boris Arturo González Rivera <borisgr04@gmail.com>
2019-04-24 21:58:21 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = 'PrimerEjemploAngular';
+  subtitle = '.NET Core + Angular CLI v7 + Bootstrap & FontAwesome + Swagger Template';   
+  datetime = Date.now();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { ModalComponent } from './modal/modal.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    NavMenuComponent,
+    HomeComponent,
+    ModalComponent,
+    UploadComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'Add', component: AddComponent },
+      { path: 'Modal', component: ModalComponent },
+      { path: 'Upload', component: UploadComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
